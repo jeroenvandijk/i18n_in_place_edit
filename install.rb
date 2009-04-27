9c32722dfbf3be686d1af6602c8932539ce85c42
Jeroen van Dijk <jeroen@noxa.nl>
2009-04-27 17:16:58 +0200

    add install script

--- install.rb
+++ install.rb
@@ -1,1 +1,3 @@
-# Install hook code here
+logger.info "Copying assets to public folder"
+File.copy File.join(File.dirname(__FILE__), "assets", "translateable.js"), File.join(RAILS_ROOT, "public", "javascripts", "translateable.js")
+File.copy File.join(File.dirname(__FILE__), "assets", "translateable.css"), File.join(RAILS_ROOT, "public", "stylesheets", "translateable.css")
