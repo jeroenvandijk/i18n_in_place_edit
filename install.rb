12bc708b121f2a5d6d3a1cd676c676571258dba0
Jeroen van Dijk <jeroen@noxa.nl>
2009-04-27 17:38:16 +0200

    Fixed install script

--- install.rb
+++ install.rb
@@ -1,3 +1,2 @@
-logger.info "Copying assets to public folder"
-File.copy File.join(File.dirname(__FILE__), "assets", "translateable.js"), File.join(RAILS_ROOT, "public", "javascripts", "translateable.js")
-File.copy File.join(File.dirname(__FILE__), "assets", "translateable.css"), File.join(RAILS_ROOT, "public", "stylesheets", "translateable.css")
+FileUtils.copy File.join(File.dirname(__FILE__), "assets", "translateable.js"), File.join(RAILS_ROOT, "public", "javascripts", "translateable.js")
+FileUtils.copy File.join(File.dirname(__FILE__), "assets", "translateable.css"), File.join(RAILS_ROOT, "public", "stylesheets", "translateable.css")
